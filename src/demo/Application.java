d50705f66bc524ad5165c410d85401d1edbf07f9
Martin Winandy <martin.winandy@pmwmedia.de>
2020-03-12 00:31:57 +0100

    Upgrade tinylog to version 2.1.0

--- src/demo/Application.java
+++ src/demo/Application.java
@@ -8,28 +8,28 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.pmw.tinylog.Configurator;
-import org.pmw.tinylog.Logger;
+import org.tinylog.Logger;
+import org.tinylog.configuration.Configuration;
 
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigValue;
 
 public class Application {
 
-	private static final Pattern PATTERN = Pattern.compile("(tinylog\\.writer\\w*)(\\.type)");
+	private static final Pattern PATTERN = Pattern.compile("(writer\\w*)(\\.type)");
 
 	public static void main(final String[] args) {
-		/* Load the type-safe configuration as stream */
-		Stream<Entry<String, ConfigValue>> stream = ConfigFactory.load("tinylog.conf").resolve().withOnlyPath("tinylog").entrySet().stream();
+		/* Load type-safe configuration as stream */
+		Stream<Entry<String, ConfigValue>> stream = ConfigFactory.load("tinylog.conf").resolve().entrySet().stream();
 
-		/* Map keys 'tinylog.writer*.type' to 'tinylog.writer*' (see reason in tinylog.conf) */
+		/* Map keys 'writer*.type' to 'writer*' (see reason in tinylog.conf) */
 		Function<String, String> keyMapper = key -> { Matcher matcher = PATTERN.matcher(key); return matcher.matches() ? matcher.group(1) : key; };
 
-		/* Convert the type-safe configuration to a tinylog compatible map */
-		Map<String, ?> map = stream.collect(Collectors.toMap(entry -> keyMapper.apply(entry.getKey()), entry -> entry.getValue().unwrapped()));
+		/* Convert type-safe configuration into a tinylog compatible map */
+		Map<String, String> map = stream.collect(Collectors.toMap(entry -> keyMapper.apply(entry.getKey()), entry -> entry.getValue().unwrapped().toString()));
 
 		/* Activate the new configuration */
-		Configurator.fromMap(map).activate();
+		Configuration.replace(map);
 
 		/* Happy logging :) */
 		Logger.info("Hello World!");
